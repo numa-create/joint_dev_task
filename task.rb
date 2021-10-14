a8e320ef71804a2ba58af844878ab60cb2f78a21
numa-create <k.numata0815@gmail.com>
2021-10-14 11:49:35 +0900

    q1<<代用修正、q2+で修正、q6出力結果問題に合わせて修正、q7-10まで回答

--- task.rb
+++ task.rb
@@ -60,7 +60,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -69,6 +69,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map(&:capitalize!)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -79,13 +81,23 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |numberK,i|
+    puts "会員No.#{i} #{numberK}"
+  end
+  
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
+  foods.each do |food|
+    if food == "うに" then
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end
 
 end
 
