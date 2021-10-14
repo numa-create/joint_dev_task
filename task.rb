38609fc944d33972e042e7b56bd1790935903b5f
numa-create <k.numata0815@gmail.com>
2021-10-14 10:39:26 +0900

    q1<<代用修正、q2+で修正、q6出力結果問題に合わせて修正

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names.<<("斎藤")
   puts names
 end
 
@@ -14,9 +14,8 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array1.push(array2)
-  array1.flatten!
-  p array1
+  array3 = array1 + array2
+  p array3
 
 end
 
@@ -53,9 +52,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  numbers1.each do |number|
-    puts number * 10
-  end
+  new_numbers = numbers1.map { |n| n * 10 }
+  p new_numbers
 end
 
 def q7
