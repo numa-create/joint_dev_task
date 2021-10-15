713b745128e0e40ec74c014557c4b1b92c3a6819
numa-create <k.numata0815@gmail.com>
2021-10-16 07:27:41 +0900

    Q15の修正とQ16の回答

--- task.rb
+++ task.rb
@@ -144,16 +144,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.has_key?(:age)
-    puts "OK"
-    else
-      puts "NG"
-  end
-  if data2.has_key?(:age)
-    puts "OK"
-    else
-      puts "NG"
-  end
+  puts (data1.has_key?(:age) ? "OK" : "NG")
+  puts (data2.has_key?(:age) ? "OK" : "NG")
 
 end
 
@@ -166,7 +158,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |value|
+    puts "私の名前は#{value [:name]}です。年齢は#{value [:age]}歳です。"
+  end
 end
 
 class UserQ17
