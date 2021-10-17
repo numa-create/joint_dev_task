a4f07ce2977fefc485ecf6098a830369b9abd5cc
numa-create <k.numata0815@gmail.com>
2021-10-18 06:55:30 +0900

    Q15/16の修正

--- task.rb
+++ task.rb
@@ -144,8 +144,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  puts (data1.has_key?(:age) ? "OK" : "NG")
-  puts (data2.has_key?(:age) ? "OK" : "NG")
+  puts data1.has_key?(:age) ? "OK" : "NG"
+  puts data2.has_key?(:age) ? "OK" : "NG"
 
 end
 
@@ -158,8 +158,8 @@ def q16
   ]
 
   # 以下に回答を記載
-  users.each do |value|
-    puts "私の名前は#{value [:name]}です。年齢は#{value [:age]}歳です。"
+  users.each do |user|
+    puts "私の名前は#{user [:name]}です。年齢は#{user [:age]}歳です。"
   end
 end
 
